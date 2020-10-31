55db6d28a99dfa52b9c6919b28877929c9f5cdc3
mtsukasa0607 <m.tsukasa0607@gmail.com>
2020-11-01 00:38:58 +0900

    デプロイパッケージの作成

--- S3Examplefunction/lambda_function.py
+++ S3Examplefunction/lambda_function.py
@@ -1,3 +1,29 @@
+import boto3
+import pyminizip
+import tempfile
+import os
+
 def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
     for rec in event['Records']:
-        print (rec['s3']['object']['key'])
+        filename = rec['s3']['object']['key']
+        obj = s3.Object(rec['s3']['bucket']['name'], filename)
+        response = obj.get()
+        tmpdir = tempfile.TemporaryDirectory()
+        fp = open(tmpdir.name + '/' + filename, 'wb')
+        fp.write(response['Body'].read())
+        fp.close()
+
+        # 暗号化
+        zipname = tempfile.mkstemp(suffix = '.zip')[1]
+        os.chdir(tmpdir.name)
+        pyminizip.compress(filename, '', zipname, 'mypassword', 0)
+
+        # S3にアップロード
+        obj = s3.Object('tsukasa-write-000', filename + '.zip')
+        response = obj.put(
+            Body = open(zipname, 'rb')
+        )
+
+        tmpdir.cleanup()
+        os.unlink(zipname)
